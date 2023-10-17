9556b1c97c0b917b355d497ff5b7e0f9b54ab98e
maha <vipmha@hotmail.com>
2023-10-17 17:06:33 +0300

    // Maha update

--- GohApp/DiscoverView.swift
+++ GohApp/DiscoverView.swift
@@ -10,8 +10,10 @@ import SwiftUI
 struct DiscoverView: View {
     // let literalColor = Color(#colorLiteral(red: 0.3589375615, green: 0.5140314698, blue: 0.455712378, alpha: 1))
     // @State var index = 0
+   
     @State var userLocation : String = " "
     @State var phoneNumber : Int = 0
+    @State var images: [String] = ["Mu1", "Mu2", "Mu3", "Mu4", "mu5", "ii"]
     
     var body: some View {
       
@@ -20,70 +22,87 @@ struct DiscoverView: View {
        
             NavigationStack {
                 
-                
+                Spacer()
                 
                 ScrollView {
-                
+                    LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], content: {
+                        ForEach(self.images, id: \.self) { image in
+                                             
+                            Image(image)
+                                                                        .resizable()
+                                                                        .frame(width:170 , height: 170)
+                                                                        .cornerRadius(20)
+                        }
+                    })
 
               
-                    VStack(spacing: 10) {
-                        Text("\(searchText)")
-
-                            VStack{
-                                
-                                HStack{
-                                  //  ZStack{
-                                        Image("Mu1")
-                                            .resizable()
-                                            .frame(width:150 , height: 150)
-                                            .cornerRadius(20)
-                                         
-                                    
-                                    
-                                    Image("Mu2")
-                                        .resizable()
-                                        .frame(width:150 , height: 150)
-                                        .cornerRadius(20)
+//                    VStack(spacing: 10) {
+//                        Text("\(searchText)")
+//
+//                            VStack{
+//                                
+//                                HStack{
+//                                  //  ZStack{
+//                                        Image("Mu1")
+//                                            .resizable()
+//                                            .frame(width:150 , height: 150)
+//                                            .cornerRadius(20)
+//                                         
+//                                    
+//                                    
+//                                    Image("Mu2")
+//                                        .resizable()
+//                                        .frame(width:150 , height: 150)
+//                                        .cornerRadius(20)
+////                                    VStack{
+////                                        Spacer()
+//                                    
+//                                    
+//                                    
+//                                    
+//                                    
+//                                    
+//                                    
+//                                    
+//                                    
+////                                    }
+//                                    
+//                                  //  }
+//                                  
+//                                }
+//                                
+//                               // Spacer()
+//                                
+//                             
+//                                   // Spacer()
+//                              
+//                                
+//                                HStack{
 //                                    VStack{
-//                                        Spacer()
+//                                       // Spacer()
+//                                   
+//                                    
+//                                            Image("Mu3")
+//                                                .resizable()
+//                                                .frame(width:150 , height: 150)
+//                                                .cornerRadius(20)
+//                                        
+//                                             
+//                                      //  Spacer()
 //                                    }
-                                    
-                                  //  }
-                                  
-                                }
-                                
-                               // Spacer()
-                                
-                             
-                                   // Spacer()
-                              
-                                
-                                HStack{
-                                    VStack{
-                                       // Spacer()
-                                   
-                                    
-                                            Image("Mu3")
-                                                .resizable()
-                                                .frame(width:150 , height: 150)
-                                                .cornerRadius(20)
-                                        
-                                             
-                                      //  Spacer()
-                                    }
-                             
-                                        Image("Mu4")
-                                            .resizable()
-                                            .frame(width:150 , height: 150)
-                                            .cornerRadius(20)
-                                          //  .frame(maxWidth: 190)
-                                        
-                                    
-                                        
-                                   // }
-                                }
-                            }
-                    }
+//                             
+//                                        Image("Mu4")
+//                                            .resizable()
+//                                            .frame(width:150 , height: 150)
+//                                            .cornerRadius(20)
+//                                          //  .frame(maxWidth: 190)
+//                                        
+//                                    
+//                                        
+//                                   // }
+//                                }
+//                            }
+//                    }
                 }
                 .navigationTitle("Discover")
                 .toolbar {
@@ -99,7 +118,7 @@ struct DiscoverView: View {
                     }
                 }
             .searchable(text: $searchText)
-            }
+            }.frame(width: 360)
            // .padding()
     }
         
---
+++ GohApp/Mytrips.swift
@@ -0,0 +1,71 @@
+//
+//  MyTrips.swift
+//  GohApp
+//
+//  Created by maha on 01/04/1445 AH.
+//
+
+import SwiftUI
+
+struct MyTrips: View {
+    var body: some View {
+        VStack(alignment: .leading,spacing: 24){
+            
+            VStack(alignment: .leading){
+                HStack{
+                    Text("My Trips")
+                    
+                        .font(.title)
+                        .bold()
+                        .foregroundColor(Color("titleGray")).padding(.vertical)
+                    Spacer()
+                    
+                    Button(action: {}){
+                        Image(systemName: "plus").foregroundColor(Color("Green")).font(.system(size: 26))
+                        
+                        
+                        
+                    }
+                }
+                Divider().padding(.vertical)
+                //  Spacer()
+                 Spacer()
+                VStack{
+                    
+                    Image(systemName: "eyes").foregroundColor(Color("Green")).font(.system(size: 50))
+                      
+                    HStack{
+                      
+                        
+                        Text("Oops you are not logged in ?  ").foregroundColor(Color("Gray")).font(.title2)
+                        
+                      
+                        Button(action: {}){
+                            Text("JOIN").foregroundColor(Color("Green"))
+                                .bold()
+                            
+                        }
+                    
+                    }.padding()
+                    //.offset(y:200)
+            
+                
+                }
+            
+                Spacer()
+                
+            } .padding(.vertical)
+            
+            
+            
+            
+        }
+        .padding()
+    
+
+    }
+}
+
+#Preview {
+    MyTrips()
+}
